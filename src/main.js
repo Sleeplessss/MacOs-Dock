4d39a309d4b2887b223b54fbc16c6bfc4df17671
sakda <sakda.pr@inet.co.th>
2021-02-24 13:58:04 +0700

    [Init]

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+// import App from "./App.vue";
+import Index from "./views/index";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App)
+createApp(Index)
   .use(store)
   .use(router)
   .mount("#app");
